b361b108e6377f27c3b5e15a58c24a899ac714b9
bradzara <bradzara93@gmail.com>
2024-07-03 10:46:20 -0700

    Add show action

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,27 @@
+import { Modal } from "./Modal"
+import { StudentsShow } from "./StudentsShow";
+import { useState } from "react";
+
 export function Content() {
+  const [isStudentShowVisible, setIsStudentShowVisible] = useState(false);
+  const [currentStudent, setCurrentStudent] = useState({});
+
+  const handleShowStudent = (student) => {
+    console.log("handleShowStudent", student);
+    setIsStudentShowVisible(true);
+    setCurrentStudent(student);
+  };
+
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsStudentShowVisible("false");
+  };
+  
   return (
     <main>
-      <h1>Welcome to React!</h1>
+      <Modal show={isStudentShowVisible} onClose={handleClose}>
+        <StudentsShow student={currentStudent} />
+      </Modal>
     </main>
   )
 }
