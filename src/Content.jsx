0d2821d6c2dceebe244334e112712685dbb54fe1
Alcy Torres <mxalcytorres@gmail.com>
2024-07-03 14:31:16 -0400

    index action

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,24 @@
+import axios from "axios";
 import { Modal } from "./Modal"
+import { StudentsIndex } from "./StudentsIndex";
 import { StudentsShow } from "./StudentsShow";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function Content() {
   const [isStudentShowVisible, setIsStudentShowVisible] = useState(false);
   const [currentStudent, setCurrentStudent] = useState({});
+
+  const [students, setStudents] = useState([]);
+
+  const handleIndexStudents = () => {
+    console.log("handleIndexStudents");          
+    axios.get("http://localhost:3000/students.json").then((response) => {
+      console.log(response.data);
+      setStudents(response.data);
+    });
+  };
+
+  useEffect(handleIndexStudents, []);
 
   const handleShowStudent = (student) => {
     console.log("handleShowStudent", student);
@@ -19,6 +33,7 @@ export function Content() {
   
   return (
     <main>
+      <StudentsIndex students={students} onShowStudent={handleShowStudent}/>
       <Modal show={isStudentShowVisible} onClose={handleClose}>
         <StudentsShow student={currentStudent} />
       </Modal>
